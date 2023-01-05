1f2a95eab34710da50c51d115f112eed9c855023
Leandro Dias <leandro.dias83@hotmail.com>
2023-01-05 09:32:27 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+// tipo de dado
+// booleanos
+
+// conversao implicita
+const numero = 456;
+const numeroString = Number ("456a");
+
+//Number()
+//String()
+console.log(numero + numeroString)
+
+
+//conversao explicita
